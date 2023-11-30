8a5e9378f70c1776ff5c9081db38e9023b34e993
java_DSA <jayprakashmcode7@gmail.com>
2023-11-30 23:51:24 +0530

    Adding the comments in the selection short code

--- Shorting/Selection_Short.java
+++ Shorting/Selection_Short.java
@@ -1,26 +1,29 @@
 package dataStructuresAndAlgorithmsDSA.Shorting;
 
 public class Selection_Short {
-
-public static void printArray(int arr[]){
-    for(int i=0;i<arr.length;i++){
-        System.out.print(arr[i]+" ");
+    // function to print an array
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
-}
+
     public static void main(String[] args) {
-        int arr[]={7,8,3,3,1,2};
-        
-        for(int i=0;i<arr.length;i++){
-            int small=i;
-            for(int j=i;j<arr.length;j++){
-                if(arr[small]>arr[j]){
-                    small=j;
+        int arr[] = { 7, 8, 3, 3, 1, 2 };
+        // Operations
+        for (int i = 0; i < arr.length; i++) {
+            int small = i; // Initializing small
+            for (int j = i; j < arr.length; j++) {
+                if (arr[small] > arr[j]) {
+                    small = j; // finding the index of smallest element
                 }
             }
-            int swap=arr[i];
-            arr[i]=arr[small];
-            arr[small]=swap;
+            // swaping the smaller element the i th index
+            int swap = arr[i];
+            arr[i] = arr[small];
+            arr[small] = swap;
         }
+        // printing the shorted array
         printArray(arr);
     }
 }
