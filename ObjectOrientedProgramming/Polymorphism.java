477dc4d847faf341f49eb1132b82a90be84cc7e9
java_DSA <jayprakashmcode7@gmail.com>
2023-12-12 10:24:18 +0530

    Adding the file of code thta contains the basics of Polymorphism

---
+++ ObjectOrientedProgramming/Polymorphism.java
@@ -0,0 +1,37 @@
+package dataStructuresAndAlgorithmsDSA.ObjectOrientedProgramming;
+
+/*
+ * Polymorphisms are of two type:- 1) Method overloading(Compile time polymorphism) 2) Method overriding(Run time polymorphism)
+ * 
+ */
+
+class Poly {
+    String name;
+    int age;
+    int password;
+    String userID;
+
+    // methods overloading (Compile time polymorphism)
+    /*
+     * Method overloading ca be done
+     */
+
+    public void printbasicInfo(String name, int age) {
+        System.out.println(name + " " + age);
+    }
+
+    public void printbasicInfo(int password) {
+        System.out.println(password);
+    }
+
+    public String printbasicInfo(String userid) {
+        return userid;
+    }
+}
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        Poly p = new Poly();
+        System.out.println(p.printbasicInfo("Jay223"));
+    }
+}
