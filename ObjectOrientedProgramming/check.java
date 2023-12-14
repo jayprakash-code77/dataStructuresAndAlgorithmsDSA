a4a729501941f64137d6f138c6d71c7a44fc20ca
java_DSA <jayprakashmcode7@gmail.com>
2023-12-14 15:13:29 +0530

    checking

---
+++ ObjectOrientedProgramming/check.java
@@ -0,0 +1,8 @@
+package dataStructuresAndAlgorithmsDSA.ObjectOrientedProgramming;
+import dataStructuresAndAlgorithmsDSA.ObjectOrientedProgramming.PackageCollege;
+public class check {
+    public static void main(String[] args) {
+        String c=PackageCollege.PackAll;
+        System.out.println(c);
+    }
+}
