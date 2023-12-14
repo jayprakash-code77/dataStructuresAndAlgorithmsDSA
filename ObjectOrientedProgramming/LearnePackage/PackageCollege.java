c74fdc50609f42eec78994864cbdc447d4f1fd5e
java_DSA <jayprakashmcode7@gmail.com>
2023-12-14 15:23:46 +0530

    Adding two more files

---
+++ ObjectOrientedProgramming/LearnePackage/PackageCollege.java
@@ -0,0 +1,9 @@
+package dataStructuresAndAlgorithmsDSA.ObjectOrientedProgramming.LearnePackage;
+
+public class PackageCollege {
+    public static String PackAll="PackedAll";
+
+    public void chacha(){
+        System.out.println("checking");
+    }
+}
