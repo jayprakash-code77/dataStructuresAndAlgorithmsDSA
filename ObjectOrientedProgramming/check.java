a5dc1fd54a7a7b2e4762562a55637be017092d52
java_DSA <jayprakashmcode7@gmail.com>
2024-01-10 01:32:48 +0530

    Adding the new file.

---
+++ ObjectOrientedProgramming/check.java
@@ -0,0 +1,11 @@
+package dataStructuresAndAlgorithmsDSA.ObjectOrientedProgramming;
+
+import dataStructuresAndAlgorithmsDSA.ObjectOrientedProgramming.LearnePackage.PackageCollege;
+
+public class check {
+    public static void main(String[] args) {
+    PackageCollege p = new PackageCollege();
+        String c=p.PackAll;
+        System.out.println(c);
+    }
+}
