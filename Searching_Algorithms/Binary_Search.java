0abb3249b0ecc3cbd5fab472bf16ba3e8d898fe7
Jaypraksh Maurya <jayprakashmcode7@gmail.com>
2024-06-01 12:53:57 +0530

    Adding commit from Intelij IDE for just testing perpose.

--- Searching_Algorithms/Binary_Search.java
+++ Searching_Algorithms/Binary_Search.java
@@ -3,5 +3,6 @@ package dataStructuresAndAlgorithmsDSA.Searching_Algorithms;
 class Binary_Search {
     public static void main(String[] args) {
         System.out.println("HEllo");
+        System.out.println("I am just testing out the InteliJ IDE");
     }
 }
