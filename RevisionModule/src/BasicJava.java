eb78f0101decad5568f4af10cb14aeb2652f1d59
Jaypraksh Maurya <jayprakashmcode7@gmail.com>
2025-02-12 13:24:53 +0530

    Adding all for for new start.

---
+++ RevisionModule/src/BasicJava.java
@@ -0,0 +1,16 @@
+public class BasicJava {
+    public static void main(String[] args) {
+//        System.out.print("Hello World");
+        short num = 4;
+        System.out.print(num+"\n");
+        byte n = 23;
+        System.out.println(n);
+
+        float nom = 2.325f;
+        System.out.println(nom);
+
+        double dl = 4.342;
+        System.out.print(dl);
+    }
+
+}
--- RevisionModule/src/Patterns.java
+++ RevisionModule/src/Patterns.java
@@ -1,5 +1,62 @@
 public class Patterns {
     public static void main(String[] args) {
         System.out.println("Jayprakash Maurya");
+
+//        printing the square
+        /*
+        for(int i=0; i<5; i++) {
+            for(int j=0;j<5;j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+         */
+
+//        printing half pyramid
+        /*
+        int n=5;
+        for(int i=0; i<5; i++) {
+            for(int j=0; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+         */
+
+
+
+//        printing the reverse pyramid
+        /*
+        for(int i=0; i<5; i++){
+            for(int j=0; j<5-i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+         */
+
+
+//        printing the reverse mirror pyramid
+        /*
+        for(int i=0; i<9; i++){
+            // space
+            for (int j=0; j<i; j++){
+                System.out.print(" ");
+            }
+
+            for (int j=0; j<9-i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+         */
+
+
+//        matrix addition
+
+
     }
 }
